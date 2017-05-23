91f421ea9335969ea21ee9f98e4c3b489bf9e553
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2017-05-23 17:20:59 +0200

    new propagator

--- crossword_problem.py
+++ crossword_problem.py
@@ -12,15 +12,13 @@ from crossword_heuer import CrosswordHeuerBrutetForce, CrosswordHeuer
 from types import SimpleNamespace
 import string
 #logging.basicConfig(level=logging.INFO)
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 
 """
     P I E R
     I D L E
     N O S E
     S L E D
-
-    A B
-    A B
 
     C A N
     A G E
@@ -29,15 +27,43 @@ import string
     M I S L E D
     U M P I R E
     S P I G O T
-    C Z R A T E
+    C U R A T E
     I R I T I C
     D E T E C T
+
+    C A L L E T
+    O P I A T E
+    M I M B A R
+    P E N I L E 
+    O C E L O T
+    S E R E N E 
 """
 
-DICTIONARY = ['PIER', 'IDLE', 'NOSE', 'SLED', 'PINS', 'IDOL', 'ELSE', 'REED', 'KEEP', 'LOVE', 'RIDE', 'BIKE']
-DICTIONARY2 = ['AB', 'AB', 'AA', 'BB']
+DICTIONARY = ['PIER', 'IDLE', 'NOSE', 'SLED', 'PINS', 'IDOL', 'ELSE', 'REED']
+DICTIONARY3 = ['AB', 'AB', 'AA', 'BB']
 DICTIONARY2 = ['CAN', 'AGE', 'ROW', 'CAR', 'AGO', 'NEW']
-DICTIONARY3 = ['MISLED', 'UMPIRE', 'SPIGOT', 'CURATE', 'IRITIC', 'DETECT', 'MUSCID', 'IMPURE', 'SPIRIT', 'LIGATE', 'EROTIC', 'DETECT']
+DICTIONARY2 = ['CALLET', 'OPIATE', 'MIMBAR', 'PENILE', 'OCELOT', 'SERENE', 'COMPOS', 'APIECE', 'LIMNER', 'LABILE', 'ETALON', 'TERETE']
+
+DICTIONARY2 = ['ABLE', 'ACID', 'AGED', 'ALSO', 'AREA', 'ARMY', 'AWAY', 'BABY', 'BACK', 'BALL', 'BAND', 'BANK', 'BASE', 'BATH', 'BEAR', 'BEAT', 'BEEN', 'BEER', 'BELL', 'BELT', 'BEST', 'BILL', 'BIRD', 'BLOW', 'BLUE', 'BOAT', 'BODY', 'BOMB', 'BOND', 'BONE', 'BOOK', 'BOOM', 'BORN', 'BOSS', 'BOTH', 'BOWL', 'BULK', 'BURN', 'BUSH', 'BUSY', 'CALL', 'CALM', 'CAME', 'CAMP', 'CARD', 'CARE', 'CASE', 'CASH', 'CAST', 'CELL', 'CHAT', 'CHIP', 'CITY', 'CLUB', 'COAL', 'COAT', 'CODE', 'COLD', 'COME', 'COOK', 'COOL', 'COPE', 'COPY', 'CORE', 'COST', 'CREW', 'CROP', 'DARK', 'DATA', 'DATE', 'DAWN', 'DAYS', 'DEAD', 'DEAL', 'DEAN', 'DEAR', 'DEBT', 'DEEP', 'DENY', 'DESK', 'DIAL', 'DICK', 'DIET', 'DISC', 'DISK', 'DOES', 'DONE', 'DOOR', 'DOSE', 'DOWN', 'DRAW', 'DREW', 'DROP', 'DRUG', 'DUAL', 'DUKE', 'DUST', 'DUTY', 'EACH', 'EARN', 'EASE', 'EAST', 'EASY', 'EDGE', 'ELSE', 'EVEN', 'EVER', 'EVIL', 'EXIT', 'FACE', 'FACT', 'FAIL', 'FAIR', 'FALL', 'FARM', 'FAST', 'FATE', 'FEAR', 'FEED', 'FEEL', 'FEET', 'FELL', 'FELT', 'FILE', 'FILL', 'FILM', 'FIND', 'FINE', 'FIRE', 'FIRM', 'FISH', 'FIVE', 'FLAT', 'FLOW', 'FOOD', 'FOOT', 'FORD', 'FORM', 'FORT', 'FOUR', 'FREE', 'FROM', 'FUEL', 'FULL', 'FUND', 'GAIN', 'GAME', 'GATE', 'GAVE', 'GEAR', 'GENE', 'GIFT', 'GIRL', 'GIVE', 'GLAD', 'GOAL', 'GOES', 'GOLD', 'GOLF', 'GONE', 'GOOD', 'GRAY', 'GREW', 'GREY', 'GROW', 'GULF', 'HAIR', 'HALF', 'HALL', 'HAND', 'HANG', 'HARD', 'HARM', 'HATE', 'HAVE', 'HEAD', 'HEAR', 'HEAT', 'HELD', 'HELL', 'HELP', 'HERE', 'HERO', 'HIGH', 'HILL', 'HIRE', 'HOLD', 'HOLE', 'HOLY', 'HOME', 'HOPE', 'HOST', 'HOUR', 'HUGE', 'HUNG', 'HUNT', 'HURT', 'IDEA', 'INCH', 'INTO', 'IRON', 'ITEM', 'JACK', 'JANE', 'JEAN', 'JOHN', 'JOIN', 'JUMP', 'JURY', 'JUST', 'KEEN', 'KEEP', 'KENT', 'KEPT', 'KICK', 'KILL', 'KIND', 'KING', 'KNEE', 'KNEW', 'KNOW', 'LACK', 'LADY', 'LAID', 'LAKE', 'LAND', 'LANE', 'LAST', 'LATE', 'LEAD', 'LEFT', 'LESS', 'LIFE', 'LIFT', 'LIKE', 'LINE', 'LINK', 'LIST', 'LIVE', 'LOAD', 'LOAN', 'LOCK', 'LOGO', 'LONG', 'LOOK', 'LORD', 'LOSE', 'LOSS', 'LOST', 'LOVE', 'LUCK', 'MADE', 'MAIL', 'MAIN', 'MAKE', 'MALE', 'MANY', 'MARK', 'MASS', 'MATT', 'MEAL', 'MEAN', 'MEAT', 'MEET', 'MENU', 'MERE', 'MIKE', 'MILE', 'MILK', 'MILL', 'MIND', 'MINE', 'MISS', 'MODE', 'MOOD', 'MOON', 'MORE', 'MOST', 'MOVE', 'MUCH', 'MUST', 'NAME', 'NAVY', 'NEAR', 'NECK', 'NEED', 'NEWS', 'NEXT', 'NICE', 'NICK', 'NINE', 'NONE', 'NOSE', 'NOTE', 'OKAY', 'ONCE', 'ONLY', 'ONTO', 'OPEN', 'ORAL', 'OVER', 'PACE', 'PACK', 'PAGE', 'PAID', 'PAIN', 'PAIR', 'PALM', 'PARK', 'PART', 'PASS', 'PAST', 'PATH', 'PEAK', 'PICK', 'PINK', 'PIPE', 'PLAN', 'PLAY', 'PLOT', 'PLUG', 'PLUS', 'POLL', 'POOL', 'POOR', 'PORT', 'POST', 'PULL', 'PURE', 'PUSH', 'RACE', 'RAIL', 'RAIN', 'RANK', 'RARE', 'RATE', 'READ', 'REAL', 'REAR', 'RELY', 'RENT', 'REST', 'RICE', 'RICH', 'RIDE', 'RING', 'RISE', 'RISK', 'ROAD', 'ROCK', 'ROLE', 'ROLL', 'ROOF', 'ROOM', 'ROOT', 'ROSE', 'RULE', 'RUSH', 'RUTH', 'SAFE', 'SAID', 'SAKE', 'SALE', 'SALT', 'SAME', 'SAND', 'SAVE', 'SEAT', 'SEED', 'SEEK', 'SEEM', 'SEEN', 'SELF', 'SELL', 'SEND', 'SENT', 'SEPT', 'SHIP', 'SHOP', 'SHOT', 'SHOW', 'SHUT', 'SICK', 'SIDE', 'SIGN', 'SITE', 'SIZE', 'SKIN', 'SLIP', 'SLOW', 'SNOW', 'SOFT', 'SOIL', 'SOLD', 'SOLE', 'SOME', 'SONG', 'SOON', 'SORT', 'SOUL', 'SPOT', 'STAR', 'STAY', 'STEP', 'STOP', 'SUCH', 'SUIT', 'SURE', 'TAKE', 'TALE', 'TALK', 'TALL', 'TANK', 'TAPE', 'TASK', 'TEAM', 'TECH', 'TELL', 'TEND', 'TERM', 'TEST', 'TEXT', 'THAN', 'THAT', 'THEM', 'THEN', 'THEY', 'THIN', 'THIS', 'THUS', 'TILL', 'TIME', 'TINY', 'TOLD', 'TOLL', 'TONE', 'TONY', 'TOOK', 'TOOL', 'TOUR', 'TOWN', 'TREE', 'TRIP', 'TRUE', 'TUNE', 'TURN', 'TWIN', 'TYPE', 'UNIT', 'UPON', 'USED', 'USER', 'VARY', 'VAST', 'VERY', 'VICE', 'VIEW', 'VOTE', 'WAGE', 'WAIT', 'WAKE', 'WALK', 'WALL', 'WANT', 'WARD', 'WARM', 'WASH', 'WAVE', 'WAYS', 'WEAK', 'WEAR', 'WEEK', 'WELL', 'WENT', 'WERE', 'WEST', 'WHAT', 'WHEN', 'WHOM', 'WIDE', 'WIFE', 'WILD', 'WILL', 'WIND', 'WINE', 'WING', 'WIRE', 'WISE', 'WISH', 'WITH', 'WOOD', 'WORD', 'WORE', 'WORK', 'YARD', 'YEAH', 'YEAR', 'YOUR', 'ZERO', 'ZONE']
+
+DICTIONARY2 = ['BRAUSE', 'RASA', 'ANONYM', 'UHUS', 'SALATE',
+              'DEN', 'LOS', 'BREZEL', 'ECK', 'NATION', 'ANHAND',
+              'SOUL', 'ANSAGE', 'ALBEN', 'ORCA', 'INSEKT', 'DIEB',
+              'REGATTA', 'BOLERO', 'RAMPE', 'MALEN', 'OFEN',
+              'EIGELB', 'ALBEREI', 'EAN', 'SULTAN', 'AKTE', 'RATE',
+              'ETAT', 'SPAT', 'APFEL', 'EIBE', 'PANGEA', 'ERN',
+              'SOLE', 'BIS', 'LATERAL', 'TAKT', 'TEEN', 'OMA',
+              'GAGA', 'SET', 'NON', 'RAPS', 'ASBEST', 'UHRWERK',
+              'ADELIGER', 'MAGD', 'GR', 'DE', 'FLAU', 'OFT', 'UMTUN',
+              'MIES', 'ROH', 'SEMINAR', 'FASELEI', 'EGO']
+
+"""
+T U N
+A   T
+T O T
+"""
+
+DICTIONARY = ['TUN', 'NUT', 'TAT', 'TOT']
 
 def getWords():
     num_words = []
@@ -45,7 +71,7 @@ def getWords():
     for word in DICTIONARY:
         num_word = []
         for l in word:
-            num = string.ascii_uppercase.index(l)
+            num = ALPHABET.index(l)
             num_word.append(num)
             if num in count.keys():
                 count[num] = count[num] + 1
@@ -66,7 +92,7 @@ def solve(row=10, col=10):
     # Variabeln für die einzelnen stimmen pro quadrat definieren
     for x in range(row):
         for y in range(col):
-            for l in range(lb_global, ub_global+1):
+            for l in range(0, len(ALPHABET)):
                 s[x, y, l] = model.addVar(vtype="B", name="{}-{}-{}".format(x, y, l))
             #s[x, y] = model.addVar(vtype="I", lb=lb_global, ub=ub_global, name="{}-{}".format(x, y))
 
@@ -74,15 +100,15 @@ def solve(row=10, col=10):
     # Nur 1 Buchstaben pro Feld
     for x in range(row):
         for y in range(col):
-            model.addCons(quicksum(s[x, y, l] for l in range(lb_global, ub_global+1)) == 1)
+            model.addCons(quicksum(s[x, y, l] for l in range(0, len(ALPHABET))) == 1)
 
     # Jeder Buchstaben muss mind 1 vorhanden ist
     # for l in words_count.keys():
     #     model.addCons(quicksum(s[x, y, l]  for x in range(col) for y in range(row)) >= 1)
 
     # Buchstabe x darf nicht mehr vorkommen als gezählt
-    for i, c in words_count.items():
-        model.addCons(quicksum(s[x, y, i]  for x in range(col) for y in range(row)) <= c)
+    # for i, c in words_count.items():
+    #     model.addCons(quicksum(s[x, y, i]  for x in range(col) for y in range(row)) <= c)
 
     # CONSTRAINTS
     # - - - - - - - - - - - - - - - - - -
@@ -91,7 +117,7 @@ def solve(row=10, col=10):
     #     for y in range(col):
     #         model.addCons(s[x, y, l] for l in range(26)) == 1)  # Nur ein Buchstaben
 
-    conshdlr = CrosswordsHdlr(DICTIONARY, lb_global, ub_global, row=row, col=col, logger=logger)
+    conshdlr = CrosswordsHdlr(DICTIONARY, 0, len(ALPHABET), row=row, col=col, logger=logger)
     random.shuffle(DICTIONARY)
     heuristic = CrosswordHeuerBrutetForce(DICTIONARY, row, logger=logger)
     #heuristic = CrosswordHeuer(DICTIONARY, row, col, lb_global, ub_global, logger=logger)
@@ -101,15 +127,16 @@ def solve(row=10, col=10):
     #                   timingmask=SCIP_HEURTIMING.BEFORENODE)
 
     model.includeConshdlr(conshdlr, "crossword",
-                          "Crossword", chckpriority=-10,
-                          enfopriority = -10, propfreq=20)
+                          "Crossword", chckpriority=-10, maxprerounds=1,
+                          enfopriority=-10, propfreq=10)
     
+    #model.setObjective(quicksum(s[x, y, 26] for x in range(col) for y in range(row)), "minimize")
 
     # Add horizontal 
     vars = []
     for x in range(row):
         for y in range(col):
-            for l in range(lb_global, ub_global+1):
+            for l in range(0, len(ALPHABET)):
                 var = s[x, y, l]
                 vars.append(var)
                 #vals = set(range(int(round(var.getLbLocal())), int(round(var.getUbLocal())) + 1))
@@ -122,7 +149,7 @@ def solve(row=10, col=10):
 
     # http://scip.zib.de/doc/html/group__ParameterMethods.php#gab2bc4ccd8d9797f1e1b2d7aaefa6500e
     #model.setEmphasis(SCIP_PARAMEMPHASIS.CPSOLVER) # No LP Relaxtion
-    model.setPresolve(SCIP_PARAMSETTING.OFF)  # Turn off presolver
+    #model.setPresolve(SCIP_PARAMSETTING.OFF)  # Turn off presolver
     model.setBoolParam("misc/allowdualreds", False)
     #model.hideOutput()
     model.optimize()
@@ -136,19 +163,19 @@ def solve(row=10, col=10):
     for x in range(row):
         out = ''
         for y in range(col):
-            for l in range(lb_global, ub_global+1):
+            for l in range(0, len(ALPHABET)):
                 #letter = 1
                 if model.getVal(s[x, y, l]) >= 0.2:
                     letter = l
-            out += "{:2} | ".format(string.ascii_uppercase[int(letter)])
+            out += "{:2} | ".format(ALPHABET[int(letter)])
         print(out)
-    
+
     del model # consfree get called
     return True
 
 
 def main():
-    count = 4
+    count = 3
     solve(row=count, col=count)
 
 if __name__ == "__main__":
--- crosswordsHandler.py
+++ crosswordsHandler.py
@@ -11,6 +11,8 @@ import time
 import string
 from types import SimpleNamespace
 
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+
 def max_var(vars):
     """
     Gibt das höchste l zurück
@@ -35,6 +37,7 @@ class CrosswordsHdlr(Conshdlr):
         self.dictionary = dictionary
         self.lb = lb
         self.ub = ub
+        self.debug = False
         self.logger = logger or logging.getLogger(__name__)
 
     def print_cross(self, cross):
@@ -53,11 +56,11 @@ class CrosswordsHdlr(Conshdlr):
         for x in range(self.row):
             row = []
             for y in range(self.col):
-                for l in range(self.lb, self.ub+1):
+                for l in range(self.lb, self.ub):
                     name = "{}-{}-{}".format(x, y, l)
                     var = [v for v in vars if v.name == name][0]
                     if self.model.getSolVal(sol, var) > 0.2:
-                        row.append(string.ascii_uppercase[l])
+                        row.append(ALPHABET[l])
 
             # Only one letter per field is allowed
             #if len(row) != self.row:
@@ -96,9 +99,12 @@ class CrosswordsHdlr(Conshdlr):
             # Create word
             builded_words.append(''.join(word))
 
-        for word in self.dictionary:
-            if word not in builded_words:
+        #builded_words = [word for word in builded_words if len(word) > 1]
+        words = self.dictionary.copy()
+        for word in builded_words:
+            if word not in words:
                 return False
+            del words[words.index(word)]
 
         return True
 
@@ -128,71 +134,208 @@ class CrosswordsHdlr(Conshdlr):
         pass
 
 #==========================================
-    def get_possible_words(self, indexes, letters):
-        words = []
-        for word in self.dictionary:
-            words.append(word)
-            for i, index in enumerate(indexes):
-                if word[int(index)] != string.ascii_uppercase[int(letters[i])]:
-                    try:
-                        del words[words.index(word)]
-                    except ValueError:
-                        pass
-                    continue
-        return words
+    # def get_possible_words(self, indexes, letters, x=True):
+    #     if len(indexes) == 0:      # All words are possible
+    #         # Give back words which can pass at this position
+    #         if x:
+    #             return [word for word in self.dictionary if len(word) <= self.row]
 
+    #         return [word for word in self.dictionary if len(word) <= self.col]
+
+    #     words = []
+    #     for word in self.dictionary:
+    #         words.append(word)
+    #         for i, index in enumerate(indexes):
+    #             if word[int(index)] != ALPHABET[int(letters[i])]:
+    #                 del words[words.index(word)]
+    #                 break
+    #     return words
+
+    def letter_is_possible_at(self, letter, row, y):
+        #print("CHECK IF LETTER {} is possible at position {} row {}".format(letter, y, row))
+        # if letter == 26:
+        #     return True
+
+        # # Check if pos-1 and pos+1 are black blocks
+        # if y-1 < 0 and len(row[y+1]) == 1 and row[y+1][0] == 26:
+        #     print("Fixed var pos 1")
+        #     return True
+
+        # if y+1 >= self.col and len(row[y-1]) == 1 and row[y-1][0] == 26:
+        #     print("Fixed var pos 1")
+        #     return True
+
+        # if y-1 >= 0 and y+1 < self.col:
+        #     if len(row[y-1]) == 1 and row[y-1][0] == 26 and len(row[y+1]) == 1 and row[y+1][0] == 26:
+        #         print("Fixed var pos 1")
+        #         return True
+
+        possible_words = []
+        for word in self.dictionary:
+            if ALPHABET[letter] in word:
+                possible_words.append(word)
+
+        # All possible Words
+        # Check if Bike can be placed
+        deleted_words = 0
+        # if self.debug:
+        #     pdb.set_trace()
+        for word in possible_words.copy():
+            #print("CHECK WORD: {}".format(word))
+            indices = [i for i, l in enumerate(word) if l == ALPHABET[letter]]
+            # if self.debug:
+            #     print("INDICES: {}".format(indices))
+            #correct = {}
+            impossibles_indices = 0
+            for idx in indices:
+                #correct[idx] = True
+                if y - idx < 0:
+                    #print("TOO MUCH LEFT: {}".format(idx))
+                    # Word is not possible with this index
+                    #correct[idx] = False
+                    impossibles_indices += 1
+                    continue
+                temp_y = y - idx
+                #print("TEMP_Y: {}".format(temp_y))
+
+                if temp_y + len(word) > self.col:
+                    #correct[idx] = False
+                    impossibles_indices += 1                    
+                    #print("TOO MUCH RIGHT: {}".format(idx))
+                    continue
+                #print("CHECK WORD")
+                for l_idx, l in enumerate(word):
+                    #print("CHECK {} is in row{} row: {}:".format(l,temp_y+l_idx, row[temp_y+l_idx]))
+                    if ALPHABET.index(l) not in row[temp_y+l_idx]:
+                        #print("L {} not in {}".format(l, row[temp_y+l_idx]))
+                        #correct[idx] = False
+                        impossibles_indices += 1     
+                        break
+
+            # if self.debug:
+            #     pdb.set_trace()
+            if impossibles_indices == len(indices):
+                deleted_words += 1
+                
+            # if not any(list(correct.values())):
+            #     deleted_words += 1
+            #     #del possible_words[possible_words.index(word)]
+
+
+        return deleted_words != len(possible_words)
+        # if len(possible_words) > 0:
+        #     return True
+        # return False
 
     def propagate_cons(self, cons):
-        reduce = False
-        solution = np.zeros([self.row, self.col])
-        solution[solution == 0] = -1
+        solution = np.zeros([self.row, self.col]).tolist()
 
-        for var in cons.data.vars:
-            x, y, l = [int(v) for v in var.name.split('-')]
-            if var.getLbLocal() >= 0.5:
-                solution[x][y] = l
-
-        #print(solution)
         for x in range(self.row):
             for y in range(self.col):
-                if solution[x][y] <= 0:  # Letter not fix try to fix it
-                    #print("Found [{}] [{}] which is not set".format(x, y))
-                    # Get indexes where letter is set
-                    indexes = np.where(solution[x] > 0)[0]
-                    possible_words = []
-                    letters = []
-                    for index in indexes:
-                        letters.append(solution[x][index])
-                    possible_words.extend(self.get_possible_words(indexes, letters))
-                    #print("Possible horizontal words: {}".format(possible_words))
+                solution[x][y] = []
 
-                    letters = []
-                    indexes = np.where(solution.T[x] > 0)[0]
-                    for index in indexes:
-                        letters.append(solution.T[x][index])
+        fixed_vars = 0
+        fff = []
+        for var in cons.data.vars:
+            x, y, l = [int(v) for v in var.name.split('-')]
+            # Not fixed
+            if var.getUbLocal() >= 0.5 and var.getLbLocal() <= 0.5:
+                solution[x][y].append(l)
 
-                    possible_words.extend(self.get_possible_words(indexes, letters))
-                    #print("Possible vertical words: {}".format(possible_words))
+            # Fixed
+            if var.getLbLocal() >= 0.5:
+                solution[x][y].append(l)
+                fff.append(l)
+                fixed_vars += 1
 
-                    if len(possible_words) == 0:
-                        print("Cut Node")
-                        return SCIP_RESULT.CUTOFF
+        # All vars are fixed
+        if fixed_vars == self.row * self.col:
+            return SCIP_RESULT.DIDNOTFIND
 
-                    for var in [v for v in cons.data.vars if v.name.startswith("{}-{}".format(x, y))]:
-                        x2, y2, l2 = [int(v) for v in var.name.split('-')]
-                        # print("check possible words: {}".format(set([l[y] for l in possible_words])))
-                        if string.ascii_uppercase[l2] not in set([l[y] for l in possible_words]):
-                            # print("Var {} ({})is not in list".format(l2, string.ascii_uppercase[l2]))
-                            self.model.chgVarUb(var, -0.0)
-                            reduce = True
+        # for x in range(self.col):
+        #     for y in range(self.row):
+        #         print("{} {}: {}".format(x, y, [string.ascii_uppercase[i] for i in solution[x][y]]))
+        for x in range(self.row):
+            for y in range(self.col):
+                possible_letters_h = []
+                row = solution[x]
+                for letter in solution[x][y]:
+                    if self.letter_is_possible_at(letter, row, y):
+                        possible_letters_h.append(letter)
 
-                        if len(possible_words) == 1 and string.ascii_uppercase[l2] in set([l[y] for l in possible_words]):
-                            self.model.chgVarLb(var, 1.0)
+                #print("H Possibles Letters for {} {} are: {}".format(x, y, [ALPHABET[i] for i in possible_letters_h]))
+                
+                possible_letters_v = []
+                # Build list for possible letters on y axis
 
-        if reduce:
-            #print("REDUCE DOM")
-            return SCIP_RESULT.REDUCEDDOM
-        return SCIP_RESULT.DIDNOTFIND
+                col = []
+                for el in range(self.col):
+                    col.append(solution[el][y])
+
+                for letter in solution[x][y]:
+                    if self.letter_is_possible_at(letter, col, x):
+                        possible_letters_v.append(letter)
+                #
+                #print("V Possibles Letters for {} {} are: {}".format(x, y, [ALPHABET[i] for i in possible_letters_v]))
+                
+                possible_letters = set(possible_letters_h).intersection(set(possible_letters_v))
+
+
+                if len(possible_letters) == 0:
+                    #print("CUTOFF SOLUTION")
+                    return SCIP_RESULT.CUTOFF
+
+                for var in [v for v in cons.data.vars if v.name.startswith("{}-{}".format(x, y))]:
+                    x2, y2, l2 = [int(v) for v in var.name.split('-')]
+                    if l2 not in possible_letters:
+                        self.model.chgVarUb(var, -0.0)
+
+                #print("Possibles Letters for {} {} are: {}".format(x, y, [ALPHABET[i] for i in possible_letters]))
+
+        #print("FINISH")
+        return SCIP_RESULT.REDUCEDDOM
+        # for i, x in enumerate(not_fixed_vars[0]):
+        #     possible_letters = []
+        #     y = not_fixed_vars[1][i]   # position of col
+        #     indexes = np.where(solution[x] >= 0)[0]
+
+        #     # horizontal
+        #     possible_words = self.get_possible_words(np.where(solution[x] >= 0)[0],
+        #                                              solution[x][np.where(solution[x] >= 0)],
+        #                                              x=True)
+        #     possible_letters_h = set([le[y] for le in possible_words])
+
+        #     # Vertical
+        #     possible_words = self.get_possible_words(np.where(solution.T[y] >= 0)[0],
+        #                                              solution.T[y][np.where(solution.T[y] >= 0)],
+        #                                              x=False)
+
+        #     possible_letters_v = set([le[x] for le in possible_words])
+        #     possible_letters = possible_letters_v.intersection(possible_letters_h)
+            
+        #     # Remove duplicate
+        #     #print(possible_letters)
+        #     possible_letters = set(possible_letters)
+        #     if len(possible_letters) == 0:
+        #         #print("No solution is possible for {}, {}".format(x, y))
+        #         return SCIP_RESULT.CUTOFF
+
+        #     # possible_letters = set([le[y] for le in possible_words]).union(set([le[x] for le in possible_words]))
+        #     #if len(not_fixed_vars[0]) < 9:
+        #     #    pdb.set_trace()
+        #     #print("Possible letters at pos {}, {} are: {}".format(x, y, possible_letters))
+        #     for var in [v for v in cons.data.vars if v.name.startswith("{}-{}".format(x, y))]:
+        #         x2, y2, l2 = [int(v) for v in var.name.split('-')]
+        #         # print("check possible words: {}".format(set([l[y] for l in possible_words])))
+        #         if string.ascii_uppercase[l2] not in possible_letters:
+        #             # print("Var {} ({})is not in list".format(l2, string.ascii_uppercase[l2]))
+        #             self.model.chgVarUb(var, -0.0)
+        #             reduce = True
+
+        #         if len(possible_words) == 1 and string.ascii_uppercase[l2] in possible_letters:
+        #             self.model.chgVarLb(var, 1.0)
+        # #pdb.set_trace()
+        # return SCIP_RESULT.REDUCEDDOM
 
     def consprop(self, constraints, nusefulconss, nmarkedconss, proptiming):
         result = SCIP_RESULT.DIDNOTFIND
